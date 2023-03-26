64c1bc62eaec276e246a3374c48a30b70feede75
tommy stevan <magui.maggie@gmail.com>
2023-03-26 11:26:08 -0300

    deploy2

--- src/main/java/com/portfolio/backend/repository/ConocimientoRepository.java
+++ src/main/java/com/portfolio/backend/repository/ConocimientoRepository.java
@@ -5,19 +5,10 @@
 package com.portfolio.backend.repository;
 
 import com.portfolio.backend.model.Conocimiento;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ConocimientoRepository extends JpaRepository<Conocimiento, Long>  {
-    @Query(value = "select * from conocimiento c where c.categoria_categoria=:category", nativeQuery=true)
-    List<Conocimiento> getConocimientoListByCategory(String category);
-    
-    @Query(value = "SELECT categoria_categoria FROM conocimiento c GROUP BY c.categoria_categoria", nativeQuery=true)
-    List<String> getCategoryList();
-    
-    Long deleteByCategoria_id(long id);
 }
